d5c5e8433e2a0e2c6b7ea75121d199beed7a324d
jpldcarvalho.eth <5263301+jpldcarvalho@users.noreply.github.com>
2023-01-24 17:07:21 +0000

    remove plus tags

--- src/contacts.ts
+++ src/contacts.ts
@@ -11,18 +11,18 @@ export const openContacts = () => {
       <img src="./assets/icons/briefcase.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Business</span>
     </div>
-    <a href="mailto:joao.carvalho+wp@tripleoak.vc" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]tripleoak.vc</a>
+    <a href="mailto:joao.carvalho@tripleoak.vc" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]tripleoak.vc</a>
     <div class="mt-2 flex items-end">
       <img src="./assets/icons/idea.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Institutional/Research</span>
     </div>
-    <a href="mailto:jpld.carvalho+wp@ziku.io" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]ziku.io</a>
+    <a href="mailto:joao.carvalho@ziku.io" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]ziku.io</a>
     <div class="mt-2 flex items-end">
       <img src="./assets/icons/user.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Personal</span>
     </div>
     <p class="text-xs mt-1 mb-1">Requires proof-of-knowledge in the subject ("PoK quack: " + subject)</p>
-    <a href="mailto:me+wp@jpldcarvalho.com" class="text-sm md:text-base text-pastel-blue underline">me[at]jpldcarvalho.com</a>
+    <a href="mailto:me@jpldcarvalho.com" class="text-sm md:text-base text-pastel-blue underline">me[at]jpldcarvalho.com</a>
   `;
 
   baseWindow("w-72 h-96 md:w-[30rem] md:h-96", content);
