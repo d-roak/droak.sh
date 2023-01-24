eb57dca3646b119c9b868371a6b1bb0051d7bfc1
jpldcarvalho.eth <5263301+jpldcarvalho@users.noreply.github.com>
2023-01-24 17:06:49 +0000

    Update contacts.ts

--- src/contacts.ts
+++ src/contacts.ts
@@ -11,12 +11,12 @@ export const openContacts = () => {
       <img src="./assets/icons/briefcase.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Business</span>
     </div>
-    <a href="mailto:joao.carvalho+wp@tripleoak.pt" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]tripleoak.pt</a>
+    <a href="mailto:joao.carvalho+wp@tripleoak.vc" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]tripleoak.vc</a>
     <div class="mt-2 flex items-end">
       <img src="./assets/icons/idea.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Institutional/Research</span>
     </div>
-    <a href="mailto:jpld.carvalho+wp@campus.fct.unl.pt" class="text-sm md:text-base text-pastel-blue underline">jpld.carvalho[at]campus.fct.unl.pt</a>
+    <a href="mailto:jpld.carvalho+wp@ziku.io" class="text-sm md:text-base text-pastel-blue underline">joao.carvalho[at]ziku.io</a>
     <div class="mt-2 flex items-end">
       <img src="./assets/icons/user.svg" class="w-6 h-6 mt-4">
       <span class="ml-2 text-lg">Personal</span>
